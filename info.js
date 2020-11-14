5568aed6b8dd43d2e71e1e27104277d04db2c801
FarelGans13 <74460091+FarelGans13@users.noreply.github.com>
2020-11-15 01:28:29 +0800

    Add files via upload

---
+++ info.js
@@ -0,0 +1,28 @@
+exports.info = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif, grupch1, grupch2) => {
+	return `⚜ ${BotName} ⚜
+  
+ʜᴀʟʟᴏ *${id.split("@s.whatsapp.net")[0]}*
+"
+Pasang Iklan? Chat Whatsapp
+
+💰ᴅᴏɴᴀsɪ ᴀɢᴀʀ ʙᴏᴛ ᴛᴇᴛᴀᴘ ᴀᴋᴛɪғ, !ᴅᴏɴᴀsɪ ᴜɴᴛᴜᴋ ʙᴇʀᴅᴏɴᴀsɪ.
+
+INFO BOT! :
+🔱 *ᴀᴜᴛʜᴏʀ*: ʀᴇᴜᴢ
+🔱 *ɢɪᴛʜᴜʙ*: https://github.com/
+🔱 *ɪɴsᴛᴀɢʀᴀᴍ*: ${instagram}
+🔱 *ʏᴏᴜᴛᴜʙᴇ*: ${youtube}
+🔱 *ᴛᴇʟᴇɢʀᴀᴍ* : ${telegram}
+🔱 *ᴡʜᴀᴛsᴀᴘᴘ* : 
+
+ʙᴏᴛ ᴀᴋᴛɪғ sᴇʟᴀᴍᴀ : ${kapanbotaktif}
+ᴊᴀɴɢᴀɴ ʟᴜᴘᴀ ғᴏʟʟᴏᴡ ɪɴsᴛᴀɢʀᴀᴍ
+ ${instagram}
+
+ ☞ᴛᴇʟᴇɢʀᴀᴍ : ${telegram}`
+╔═╗═════╔╗════╔╗═╔╦╦╗
+║║╠═╦═╦═╬╣╔╦╦╦╣╠╗║║║║
+║║║╬║╬║╬║║║║║║║═╣╠╬╬╣
+╚╩╬╗╠═╣╔╩╝╠╗╠═╩╩╝╚╩╩╝
+══╚═╝═╚╝══╚═╝
+}
