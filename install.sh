defcd0627fbfc85a1f6dd9bb8b588a3b4cf31e8c
FarelGans13 <74460091+FarelGans13@users.noreply.github.com>
2020-11-15 02:10:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-apt update
+
+
 apt upgrade
-pkg install wget
-pkg install ffmpeg
-pkg install nodejs
+apt install
+apt install wget -y
+apt install ffmpeg -y
+apt install nodejs -y
 npm i -g cwebp
 npm i -g ytdl 
 npm i
-npm i got
